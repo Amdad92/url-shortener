d7cf5e0978cb8a1fc0accf5eb760c8a0984ed206
Amdad92 <emdadctg92@gmail.comgit config --global user.email>
2025-05-06 21:17:08 +0600

    add unique constrain for orginal url

--- database/migrations/2025_05_06_095932_create_links_table.php
+++ database/migrations/2025_05_06_095932_create_links_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('links', function (Blueprint $table) {
             $table->id();
-            $table->string('original_url');
+            $table->string('original_url')->unique();
             $table->string('shortened_url')->unique();
             $table->timestamps();
         });
